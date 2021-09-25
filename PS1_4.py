32d3ed5589cc72f16b630936b1443361dbaca649
zuoxx <923039947@qq.com>
2021-09-25 17:02:28 +0800

    1-4question .py

---
+++ PS1_4.py
@@ -0,0 +1,18 @@
+import random
+
+
+def Least_moves(x):
+    i = 1
+    while x != 2:
+        if x % 2 != 0:
+            x -= 1
+            i += 1
+        else:
+            x /= 2
+            i += 1
+    print(i)
+
+
+b = random.randint(2, 100)
+print(b)
+Least_moves(b)
