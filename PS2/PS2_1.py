68aca83ab265e19d797fbc833459558378344db5
zuoxx <923039947@qq.com>
2021-10-19 09:21:04 +0800

    PS_2 start

---
+++ PS2/PS2_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# import numpy
+
+f = open("Sig_Eqs.tsv","r",encoding='utf-8')
+print(f.readlines())
+
+f.close()
